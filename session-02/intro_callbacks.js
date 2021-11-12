44cf6a64fec176d76097c98b92e21ca06624eb68
German <german.mtz@protonmail.com>
2021-11-11 19:02:54 -0600

    add filter exercise solution

--- session-02/intro_callbacks.js
+++ session-02/intro_callbacks.js
@@ -1,31 +1,38 @@
-console.log("Intro Callbacks")
+console.log('Intro Callbacks')
 
 //Implementar filter a mano
 //Function toma como argumento un arreglo
 //Function toma un callback
 
 const filterArray = (array, callback) => {
-    const newArray = []
+  const newArray = []
 
-    for (let index = 0; index < array.length; index++) {
-        if (callback(array[index])) {
-            newArray.push(array[index])
-        }
+  for (let index = 0; index < array.length; index++) {
+    if (callback(array[index])) {
+      newArray.push(array[index])
     }
+  }
 
-    return newArray
+  return newArray
 }
 
-//Los callbacks debe recibir un valor, que sera un elemento del arreglo. 
+//Los callbacks debe recibir un valor, que sera un elemento del arreglo.
 
-const resultado1 = filterArray([1,2,4,7,8,10,15,65,30], x => x % 2 === 0)
+const resultado1 = filterArray(
+  [1, 2, 4, 7, 8, 10, 15, 65, 30],
+  (x) => x % 2 === 0
+)
 
-const resultado2 = filterArray(['a','b','c','d', 1], x => x === 'a' )
+const resultado2 = filterArray(['a', 'b', 'c', 'd', 1], (x) => x === 'a')
 
-const resultado3 = filterArray([1,2,3,4,5], x => x === 10)
+const resultado3 = filterArray([1, 2, 3, 4, 5], (x) => x === 10)
 
-const resultado4 = filterArray([1,"a",2,"b",3,"c",4,"d",5,"e"],
-x => typeof x === "string")
+const resultado4 = filterArray(
+  [1, 'a', 2, 'b', 3, 'c', 4, 'd', 5, 'e'],
+  (x) => typeof x === 'string'
+)
 
-const resultado5 = filterArray([1,"a",2,"b",3,"c",4,"d",5,"e"],
-    x => typeof x === "number")
+const resultado5 = filterArray(
+  [1, 'a', 2, 'b', 3, 'c', 4, 'd', 5, 'e'],
+  (x) => typeof x === 'number'
+)
